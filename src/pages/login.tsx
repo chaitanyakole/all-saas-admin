49efcebec20129687b1133685fbf177354a86105
chaitanyakole <chaitanya.kole@tekditechnologies.com>
2025-01-29 18:15:36 +0530

    Changes in code

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,18 +4,10 @@ import Loader from "../components/Loader";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
-import { showToastMessage } from "@/components/Toastify";
-import { getUserDetailsInfo } from "@/services/UserList";
-import { Storage } from "@/utils/app.constant";
-import useSubmittedButtonStore from "@/utils/useSharedState";
-import { Role } from "@/utils/app.constant";
 
 const LoginPage = () => {
   const { t } = useTranslation();
   const router = useRouter();
-  const setAdminInformation = useSubmittedButtonStore(
-    (state: any) => state.setAdminInformation
-  );
 
   useEffect(() => {
     if (typeof window !== "undefined" && window.localStorage) {
@@ -26,39 +18,6 @@ const LoginPage = () => {
     }
   }, []);
 
-  const fetchUserDetail = async () => {
-    let userId;
-    try {
-      if (typeof window !== "undefined" && window.localStorage) {
-        userId = localStorage.getItem(Storage.USER_ID);
-      }
-      const fieldValue = true;
-      if (userId) {
-        const response = await getUserDetailsInfo(userId, fieldValue);
-
-        const userInfo = response?.userData;
-        if (typeof window !== "undefined" && window.localStorage) {
-          localStorage.setItem("adminInfo", JSON.stringify(userInfo));
-          localStorage.setItem("stateName", userInfo?.customFields[0]?.value);
-        }
-
-        if (userInfo.tenantData?.[0]?.roleName === Role.LOGIN_LEARNER) {
-          const errorMessage = t("LOGIN_PAGE.ACCESS_DENIED");
-          showToastMessage(errorMessage, "error");
-          localStorage.removeItem("token");
-        } else {
-          setAdminInformation(userInfo);
-          router.push("/tenant");
-        }
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchUserDetail();
-  }, []);
   return (
     <Box sx={{ backgroundColor: "white", height: "100vh" }}>
       <Loader showBackdrop={true} loadingText={t("COMMON.LOADING")} />
