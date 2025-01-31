a7d63710d49292cb629d2d69ebb5ee601e47d922
chaitanyakole <chaitanya.kole@tekditechnologies.com>
2025-01-31 17:17:33 +0530

    Changes in keycloak

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import keycloak from "../utils/keycloak";
+import keycloak, { initKeycloak } from "../utils/keycloak";
 import Loader from "../components/Loader";
 import { Box } from "@mui/material";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
@@ -9,10 +9,10 @@ import { useTranslation } from "next-i18next";
 const LoginPage = () => {
   const { t } = useTranslation();
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loginWithKeycloak = async () => {
-      if (typeof window === "undefined" || !keycloak) return;
       try {
         const token = localStorage.getItem("token");
         if (token) {
@@ -20,29 +20,31 @@ const LoginPage = () => {
           return;
         }
 
-        // Initialize Keycloak and login
-        const authenticated = await keycloak.init({
-          onLoad: "login-required",
-          redirectUri: `${window.location.origin}/tenant`,
-        });
-
+        const authenticated = await initKeycloak();
         if (authenticated) {
-          localStorage.setItem("token", keycloak.token || "");
+          router.push("/tenant");
+        } else {
+          console.error("User is not authenticated");
         }
       } catch (error) {
         console.error("Error logging in with Keycloak:", error);
+      } finally {
+        setLoading(false);
       }
     };
-    console.log("login page called");
 
     loginWithKeycloak();
   }, []);
 
-  return (
-    <Box sx={{ backgroundColor: "white", height: "100vh" }}>
-      <Loader showBackdrop={true} loadingText={t("COMMON.LOADING")} />
-    </Box>
-  );
+  if (loading) {
+    return (
+      <Box sx={{ backgroundColor: "white", height: "100vh" }}>
+        <Loader showBackdrop={true} loadingText={t("COMMON.LOADING")} />
+      </Box>
+    );
+  }
+
+  return null;
 };
 
 export async function getStaticProps({ locale }: any) {
