fde220d1a3c08a61b85b21d472e6d64fbaad135b
chaitanyakole <chaitanya.kole@tekditechnologies.com>
2025-02-03 14:12:57 +0530

    Changes keycloak, login page and logout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,28 +1,52 @@
-import { Box } from "@mui/material";
-import React, { useEffect } from "react";
-import Loader from "../components/Loader";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import keycloak, { initKeycloak } from "../utils/keycloak";
+import Loader from "../components/Loader";
+import { Box } from "@mui/material";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
+import { registerUser } from "@/services/LoginService";
 
 const LoginPage = () => {
   const { t } = useTranslation();
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (typeof window !== "undefined" && window.localStorage) {
-      const token = localStorage.getItem("token");
-      if (token) {
-        router.push("/tenant");
+    const loginWithKeycloak = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        if (token) {
+          router.push("/tenant");
+          return;
+        }
+
+        const authenticated = await initKeycloak();
+        if (authenticated) {
+          await registerUser();
+          router.push("/tenant");
+        } else {
+          console.error("User is not authenticated");
+        }
+      } catch (error) {
+        console.error("Error logging in with Keycloak:", error);
+      } finally {
+        setLoading(false);
       }
-    }
+    };
+
+    loginWithKeycloak();
   }, []);
 
-  return (
-    <Box sx={{ backgroundColor: "white", height: "100vh" }}>
-      <Loader showBackdrop={true} loadingText={t("COMMON.LOADING")} />
-    </Box>
-  );
+  if (loading) {
+    return (
+      <Box sx={{ backgroundColor: "white", height: "100vh" }}>
+        <Loader showBackdrop={true} loadingText={t("COMMON.LOADING")} />
+      </Box>
+    );
+  }
+
+  return null;
 };
 
 export async function getStaticProps({ locale }: any) {
--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -1,49 +1,48 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Loader from "@/components/Loader";
 import { useTranslation } from "react-i18next";
 import { logout } from "@/utils/keycloak";
+
 function Logout() {
   const router = useRouter();
   const { t } = useTranslation();
+
   useEffect(() => {
     const userLogout = async () => {
       try {
         await logout();
+        if (typeof window !== "undefined" && window.localStorage) {
+          const keysToKeep = [
+            "preferredLanguage",
+            "mui-mode",
+            "mui-color-scheme-dark",
+            "mui-color-scheme-light",
+            "hasSeenTutorial",
+          ];
+
+          const valuesToKeep: { [key: string]: any } = {};
+          keysToKeep.forEach((key) => {
+            valuesToKeep[key] = localStorage.getItem(key);
+          });
+
+          localStorage.clear();
+
+          keysToKeep.forEach((key) => {
+            if (valuesToKeep[key] !== null) {
+              localStorage.setItem(key, valuesToKeep[key]);
+            }
+          });
+        }
+        router.push("/login");
       } catch (error) {
-        console.log(error);
+        console.error("Error during logout:", error);
       }
     };
+
     userLogout();
-    if (typeof window !== "undefined" && window.localStorage) {
-      // Specify the keys you want to keep
-      const keysToKeep = [
-        "preferredLanguage",
-        "mui-mode",
-        "mui-color-scheme-dark",
-        "mui-color-scheme-light",
-        "hasSeenTutorial",
-      ];
-      // Retrieve the values of the keys to keep
-      const valuesToKeep: { [key: string]: any } = {};
-      keysToKeep.forEach((key: string) => {
-        valuesToKeep[key] = localStorage.getItem(key);
-      });
-
-      // Clear all local storage
-      localStorage.clear();
-
-      // Re-add the keys to keep with their values
-      keysToKeep.forEach((key: string) => {
-        if (valuesToKeep[key] !== null) {
-          // Check if the key exists and has a value
-          localStorage.setItem(key, valuesToKeep[key]);
-        }
-      });
-    }
-  }, []);
+  }, [router]);
 
   return <Loader showBackdrop={true} loadingText={t("COMMON.LOADING")} />;
 }
